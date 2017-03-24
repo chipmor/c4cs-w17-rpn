ea590e5e0d4182c3f63d752a47f68f1eb29c1d01
Chip M <chipmor@umich.edu>
2017-03-24 12:26:07 -0400

    testing

--- rpn.py
+++ rpn.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import readline
 import operator
-
-
+from clint.textui import colored
+import sys
 OPERATORS = {
 	'+': operator.add,
 	'-': operator.sub,
@@ -13,7 +14,7 @@ OPERATORS = {
 
 def calculate(arg):
 	stack = list()
-
+	op = ''
 	for operand in arg.split():
 	
 		try:	
@@ -25,16 +26,20 @@ def calculate(arg):
 			arg1 = stack.pop()
 
 			operator_fn = OPERATORS[operand]
+			op = operand
 			result = operator_fn(arg1, arg2)
 
 			stack.append(result)
+			sys.stdout.write(colored.blue(arg1)+ ' ' + str(colored.blue(op)) + ' ' + colored.blue(arg2))
 	return stack.pop()
 
 def main():
 	while True:
 		result = calculate(input("rpn calc>"))
-		print("Result:", result)
-
+		print(colored.green(' = '+ str(result)))
+def random_func():
+	for i in range(20):
+		print('hi')
 #this has been called directly from the terminal (not from an impot
 if __name__ == '__main__':
 	main()
